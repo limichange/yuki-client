9afdf3d50b6ed68141efd13b5d7072eefd21c82f
limichange <limichange@hotmail.com>
2016-12-23 12:03:44 +0800

    update App.vue sass & pug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    router-view
 </template>
 
 <script>
@@ -11,8 +12,4 @@
 <style lang="sass">
   #app
     font-family: 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-    color: #2c3e50
-    margin-top: 60px
 </style>
