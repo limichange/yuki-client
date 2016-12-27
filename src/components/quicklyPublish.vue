0132266362baa6d215ce6e0dd2ad297e8c351dfc
limichange <limichange@hotmail.com>
2016-12-27 13:42:21 +0800

    🚧   - update update

---
+++ src/components/quicklyPublish.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  feed.quicklyPublish(v-if="me.isLogIn")
+    img.avatar(:src="me.avatar")
+    i-input(@keydown.enter="submit", placeholder="有什么新鲜事？")
+</template>
+
+<script>
+  import button from './button'
+  import input from './input'
+  import feed from './feed'
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'quicklyPublish',
+    computed: mapState([
+      'me'
+    ]),
+    components: {
+      feed,
+      'iButton': button,
+      'iInput': input
+    },
+    methods: {
+      submit () {
+        console.log('submit')
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  .quicklyPublish
+    text-align: center
+    display: flex
+
+    .avatar
+      width: 36px
+      height: 36px
+      border-radius: 100%
+      margin-right: 10px
+</style>
--- src/views/indexView.vue
+++ src/views/indexView.vue
@@ -1,29 +1,19 @@
 <template lang="pug">
   .indexView
     feeds
-      feed.quicklyPublish(v-if="me.isLogIn")
-        img.avatar(:src="me.avatar")
-        i-input(placeholder="有什么新鲜事？")
+      quickly-publish
 </template>
 
 <script>
   import feeds from '../components/feeds'
-  import feed from '../components/feed'
-  import button from '../components/button'
-  import input from '../components/input'
-  import { mapState } from 'vuex'
+  import quicklyPublish from '../components/quicklyPublish'
 
   export default {
     name: 'indexView',
     components: {
-      feed,
       feeds,
-      'iButton': button,
-      'iInput': input
-    },
-    computed: mapState([
-      'me'
-    ])
+      quicklyPublish
+    }
   }
 </script>
 
@@ -31,16 +21,4 @@
   .indexView
     width: 100%
     padding-top: 20px
-
-  .quicklyPublish
-    text-align: center
-    display: flex
-
-    .avatar
-      margin-right: 10px
-
-  .avatar
-    width: 36px
-    height: 36px
-    border-radius: 100%
 </style>
